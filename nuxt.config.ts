7240c759952cb17ebaff532617686512cde663e4
amplitudes. <hey@amplitudes.me>
2023-10-20 16:18:21 +0100

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    baseURL: './',
+    buildAssetsDir: '/',
+  },
+  runtimeConfig: {
+    app: {
+      baseURL: './',
+      buildAssetsDir: '/',
+    },
+  },
+});
